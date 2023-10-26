1d5ee01a7e1b0e78d46af54c661c95982d6004f7
Mu <mu52@ucsb.edu>
2023-10-26 15:12:22 -0700

    updated

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -47,8 +47,8 @@ public class UCSBOrganizationController extends ApiController {
         @Parameter(name="orgCode") @RequestParam String orgCode,
         @Parameter(name="orgTranslationShort") @RequestParam String orgTranslationShort,
         @Parameter(name="orgTranslation") @RequestParam String orgTranslation,
-        @Parameter(name="inactive") @RequestParam boolean inactive,
-        )
+        @Parameter(name="inactive") @RequestParam boolean inactive
+    )
         {
 
 
