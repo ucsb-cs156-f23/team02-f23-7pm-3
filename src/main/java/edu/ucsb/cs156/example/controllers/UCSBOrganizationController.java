e299fd4db6dd4c3c1b8e1ac99e2cd5c5ab957c33
Mu <mu52@ucsb.edu>
2023-10-26 19:48:00 -0700

    updated

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -1,13 +1,18 @@
+
+
 package edu.ucsb.cs156.example.controllers;
+
 
 import edu.ucsb.cs156.example.entities.UCSBOrganization;
 import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.UCSBOrganizationRepository;
 
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,7 +25,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import javax.validation.Valid;
+
 
 @Tag(name = "UCSBOrganization")
 @RequestMapping("/api/ucsborganization")
@@ -37,9 +44,10 @@ public class UCSBOrganizationController extends ApiController {
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
     public Iterable<UCSBOrganization> allOrganizations() {
-        Iterable<UCSBDOrganization> organization = ucsbOrganizationRepository.findAll();
+        Iterable<UCSBOrganization> organization = ucsbOrganizationRepository.findAll();
         return organization;
     }
+
     @Operation(summary= "Create a new organization")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/post")
@@ -48,20 +56,31 @@ public class UCSBOrganizationController extends ApiController {
         @Parameter(name="orgTranslationShort") @RequestParam String orgTranslationShort,
         @Parameter(name="orgTranslation") @RequestParam String orgTranslation,
         @Parameter(name="inactive") @RequestParam boolean inactive
-    )
+        )
         {
 
 
         UCSBOrganization organization = new UCSBOrganization();
-        organization.setorgCode(orgCode);
-        organization.setorgTranslationShort(orgTranslationShort);
-        organization.setorgTranslation(orgTranslation);
-        organization.setinactive(inactive);
+        organization.setOrgCode(orgCode);
+        organization.setOrgTranslationShort(orgTranslationShort);
+        organization.setOrgTranslation(orgTranslation);
+        organization.setInactive(inactive);
 
 
-        UCSOrganization savedOrganization = ucsbOrganizationRepository.save(organization);
+        UCSBOrganization savedOrganization = ucsbOrganizationRepository.save(organization);
 
 
         return savedOrganization;
+    }
+
+    @Operation(summary= "Get a single organization")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public UCSBOrganization getById(
+            @Parameter(name="orgCode") @RequestParam String orgCode) {
+        UCSBOrganization organization = ucsbOrganizationRepository.findById(orgCode)
+                .orElseThrow(() -> new EntityNotFoundException(UCSBOrganization.class, orgCode));
+
+        return organization;
     }
 }
